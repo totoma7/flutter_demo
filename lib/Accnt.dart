a96be4e2d8c3802dfde15cf3d982b8a1debfc400
totoma <ilchul.jung@gmail.com>
2021-09-12 21:06:19 +0900

    1차 저장 사진 편집

--- lib/Accnt.dart
+++ lib/Accnt.dart
@@ -1,45 +1,203 @@
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'main.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:image_save/image_save.dart';
+
 
 class Accnt extends StatelessWidget {
-  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ImageCropper',
+      theme: ThemeData.light().copyWith(primaryColor: Colors.deepOrange),
+      home: MyHomePage(
+        title: 'ImageCropper',
+      ),
+    );
+  }
+}
 
-  List _pages=[0,1,2];
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({required this.title});
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+enum AppState {
+  free,
+  picked,
+  cropped,
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late AppState state;
+  File? imageFile;
+  late Uint8List _data;
+  var _result ='';
+  @override
+  void initState() {
+    super.initState();
+    state = AppState.free;
+  }
+  Future<void> _saveImage(Uint8List data,String filename) async {
+    bool success = false;
+    try {
+      success = (await ImageSave.saveImage(data, filename, albumName: "demo",overwriteSameNameFile: true))!;
+    } on PlatformException catch (e, s) {
+      print(e);
+      print(s);
+    }
+    setState(() {
+      _result = success ? "Save to album success" : "Save to album failed";
+      print(_result);
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.cyan[100],
       appBar: AppBar(
-        backgroundColor: Colors.red,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            color: Colors.black,
+        title: Text(widget.title),
+      ),
+      body:  Center(
+        child: imageFile != null ? Image.file(imageFile!) : Container(),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.deepOrange,
             onPressed: () {
-              // 로그아웃
-              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (BuildContext context) => MyApp()), (route) => false);
+              if (state == AppState.free)
+                _pickImage();
+              else if (state == AppState.picked)
+                _cropImage();
+              else if (state == AppState.cropped) _clearImage();
             },
-          )
-        ],
-        title: Text('영수증'),
+            child: _buildButtonIcon(),
+          ),
+          SizedBox(width: 20,),
+          FloatingActionButton(
+            backgroundColor: Colors.deepOrange,
+            onPressed: () {
+              if (state == AppState.free)
+                _cameraImage();
+              else if (state == AppState.picked)
+                _cropImage();
+              else if (state == AppState.cropped)
+                _clearImage();
+            },
 
-      ),
-      body: Center(
-        child: Text('영수증'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.amber,
-        onPressed: (){
-          // _getImage();
-        },
-        child: Icon(Icons.add,color: Colors.black,),
+            child: _buildcameraButtonIcon(),
+          ),
+        ],
       ),
     );
   }
 
+  Widget _buildButtonIcon() {
+    if (state == AppState.free)
+      return Icon(Icons.photo_album);
+    else if (state == AppState.picked)
+      return Icon(Icons.crop);
+    else if (state == AppState.cropped)
+      return Icon(Icons.clear);
+    else
+      return Container();
+  }
+  Widget _buildcameraButtonIcon() {
+    if (state == AppState.free)
+      return Icon(Icons.camera);
+    else if (state == AppState.picked)
+      return Icon(Icons.crop);
+    else if (state == AppState.cropped)
+      return Icon(Icons.clear);
+    else
+      return Container();
+
+  }
+
+  Future<Null> _pickImage() async {
+    final pickedImage =
+    await ImagePicker().getImage(source: ImageSource.gallery);
+    imageFile = pickedImage != null ? File(pickedImage.path) : null;
+    if (imageFile != null) {
+      setState(() {
+        state = AppState.picked;
+      });
+    }
+    _cropImage();
+  }
+  Future<Null> _cameraImage() async {
+    final pickedImage =
+    await ImagePicker().getImage(source: ImageSource.camera);
+    imageFile = pickedImage != null ? File(pickedImage.path) : null;
+    if (imageFile != null) {
+      setState(() {
+        state = AppState.picked;
+      });
+    }
+    _cropImage();
+  }
+
+  Future<Null> _cropImage() async {
+    File? croppedFile = await ImageCropper.cropImage(
+        sourcePath: imageFile!.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ]
+            : [
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio5x3,
+          CropAspectRatioPreset.ratio5x4,
+          CropAspectRatioPreset.ratio7x5,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          title: 'Cropper',
+        ));
+    if (croppedFile != null) {
+      imageFile = croppedFile;
+      _saveImage(imageFile!.readAsBytesSync(),"data4.png");
+      setState(() {
+        state = AppState.cropped;
+      });
+    }
+  }
+
+  void _clearImage() {
+    imageFile = null;
+    setState(() {
+      state = AppState.free;
+    });
+  }
 }
-
-
